bf021d34928ca718235309a8429bc41cda72d671
on3g4i <d3887269@gmail.com>
2025-05-22 23:43:10 -0300

    atividade dia 15/05

--- atv_migrations/database/migrations/2025_05_23_024202_create_exercicio_table.php
+++ atv_migrations/database/migrations/2025_05_23_024202_create_exercicio_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -20,7 +19,18 @@ return new class extends Migration
 
             $table->timestamps();
         });
-
+        Schema::create('venda', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+        Schema::create('item_venda', function (Blueprint $table) {
+            $table->id();
+            $table->float('preco_praticado', 2);
+            $table->integer('qtd');
+            $table->foreignId('produto_id')->constrained('produto');
+            $table->foreignId('venda_id')->constrained('venda');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -29,5 +39,8 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('produto');
+        Schema::dropIfExists('venda');
+        Schema::dropIfExists('item_venda');
+
     }
 };
